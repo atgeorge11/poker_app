0bcb6c9e629d12dba6c2acc92766b60babe852dc
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2020-04-11 14:00:52 -0500

    sockets finished

--- poker_app/game/consumers.py
+++ poker_app/game/consumers.py
@@ -1,16 +1,44 @@
 import json
-from channels.generic.websocket  import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
-        print("accepting")
+        self.game_name = self.scope['url_route']['kwargs']['game_name']
+        self.game_group_name = 'game_%s' % self.game_name
+
+        #Join game group
+        async_to_sync(self.channel_layer.group_add)(
+            self.game_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        #Leave game group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.game_group_name,
+            self.channel_name
+        )
 
+    #Receive message from WebSocket
     def receive(self, text_data):
         data_json = json.loads(text_data)
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.game_group_name,
+            {
+                'type': 'message',
+                'message': data_json
+            }
+        )
+
+    #Receive message from room group
+    def message(self, event):
+        message = event['message']
+
+        #Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': data_json
+            'message': message
         }))
--- poker_app/poker_app/settings.py
+++ poker_app/poker_app/settings.py
@@ -76,6 +76,15 @@ WSGI_APPLICATION = 'poker_app.wsgi.application'
 
 ASGI_APPLICATION = 'poker_app.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)]
+        }
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
